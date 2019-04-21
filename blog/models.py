966c523eadb493fe6daf7ed77edd644f3add0129
Golorcsh <c1045132383@gmail.com>
2019-04-21 22:35:18 +0800

    增加分页设置 使用Paginator模块 出现BUG： 由于blog_with_type  继承 blog_list页面 blog_list页面循环展示的列表有blogs换成page_of_blogs.object.list来遍历现实当前页面的博客 而blog_with_type没有这个参数，所以无法现实

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+import ckeditor
 # Create your models here.
 
 
@@ -20,3 +21,6 @@ class Blog(models.Model):
 
     def __str__(self):
         return "<Blog: %s>" % self.title
+
+    class Meta:
+        ordering = ['-create_date']
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-from blog.models import Blog,BlogType
+from django.core.paginator import Paginator
+from blog.models import Blog, BlogType
 # Create your views here.
 
 
 def blog_list(request):
+
+    blog_all_list = Blog.objects.all()
+    # 每10个blog一页
+    paginaotr = Paginator(blog_all_list, 10)
+    # 获取url的页码参数
+    page_num = request.GET.get('page', 1)
+    page_of_blogs = paginaotr.get_page(page_num)
+
+
+
     content = {}
-    content['blogs']= Blog.objects.all()
+    content['blogs']= page_of_blogs.object_list
+    content['page_of_blogs'] = page_of_blogs
     content['blogs_count'] = Blog.objects.all().count()
     content['blog_types'] = BlogType.objects.all()
     return render(request, 'blog_list.html', content)
@@ -19,8 +31,10 @@ def blog_detail(request, blog_pk):
 
 def blog_with_type(request, blog_type_pk):
     content = {}
+    # 获取blog分类的类型
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
     content['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    # 通过blog的类型筛选出所有同一个类型的blog
     content['blogs_count'] = Blog.objects.filter(blog_type=blog_type).count()
     content['blog_type'] = blog_type
     content['blog_types'] = BlogType.objects.all()
