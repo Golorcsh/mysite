8ad81111f87deff089e29916fff68335c03dde33
Golorcsh <c1045132383@gmail.com>
2019-04-22 11:26:27 +0800

    修复分类现实问题 完善页码设置 增加日期归档分类 将blog.views通用代码代码抽出，写成一个方法

--- blog/views.py
+++ blog/views.py
@@ -3,42 +3,79 @@ from django.core.paginator import Paginator
 from blog.models import Blog, BlogType
 # Create your views here.
 
-
-def blog_list(request):
-
-    blog_all_list = Blog.objects.all()
-    # 每10个blog一页
-    paginaotr = Paginator(blog_all_list, 10)
-    # 获取url的页码参数
-    page_num = request.GET.get('page', 1)
-    page_of_blogs = paginaotr.get_page(page_num)
+EachPageNumOfBlog = 6
 
 
+def get_blog_list_common_date(request, blogs_all_list):
+
+    '''
+    公共方法
+    :param request:
+    :param blogs_all_list:
+    :return: content
+    '''
 
     content = {}
-    content['blogs']= page_of_blogs.object_list
+    # 每10个blog一页
+    paginaotr = Paginator(blogs_all_list, EachPageNumOfBlog)
+    # 获取url的页码参数
+    page_num = int(request.GET.get('page', 1))
+    page_of_blogs = paginaotr.get_page(page_num)
+    # 当前页码的前后3页的范围
+    page_range = [x for x in range(int(page_num)-2, int(page_num)+3) if 0 < x <= paginaotr.num_pages]
+    print(page_range)
+    if page_range[0]-1 >= 2:
+        page_range.insert(0, "...")
+    if paginaotr.num_pages - page_range[-1] >= 2:
+        page_range.append('...')
+    # 添加首页和尾页
+    if page_range[0] != 1 :
+        page_range.insert(0, 1)
+    if page_range[-1] != paginaotr.num_pages:
+        page_range.append(paginaotr.num_pages)
+
+    content['blogs'] = page_of_blogs
     content['page_of_blogs'] = page_of_blogs
+    content['page_range'] = page_range
     content['blogs_count'] = Blog.objects.all().count()
     content['blog_types'] = BlogType.objects.all()
+    content['blog_dates'] = Blog.objects.dates('create_date', 'month', order='DESC')
+    return content
+
+
+def blog_list(request):
+    blog_all_list = Blog.objects.all()
+    content = get_blog_list_common_date(request, blog_all_list)
     return render(request, 'blog_list.html', content)
+
+
+def blog_with_type(request, blog_type_pk):
+    # 获取blog分类的类型
+    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
+    blog_all_list = Blog.objects.filter(blog_type=blog_type)
+    content = get_blog_list_common_date(request, blog_all_list)
+    # 通过blog的类型筛选出所有同一个类型的blog
+    content['blog_type'] = blog_type
+    return render(request, 'blog_type.html', content)
+
+
+def blog_with_date(request, year, month):
+    content = {}
+    # 获取blog分类的类型
+    blog_all_list = Blog.objects.filter(create_date__year=year, create_date__month=month)
+    content = get_blog_list_common_date(request, blog_all_list)
+
+    # 通过blog的类型筛选出所有同一个类型的blog
+    content['blog_date'] = '%s年%s月' % (year, month)
+    content['blog_dates'] = Blog.objects.dates('create_date', 'month', order='DESC')
+    return render(request, 'blog_date.html', content)
 
 
 def blog_detail(request, blog_pk):
     content= {}
-    content['blog']=get_object_or_404(Blog, pk=blog_pk)
+    blog = get_object_or_404(Blog, pk=blog_pk)
+    # 通过时间筛选出前一篇和后一篇博客对象
+    content['previous_blog'] = Blog.objects.filter(create_date__lt=blog.create_date).first()
+    content['next_blog'] = Blog.objects.filter(create_date__gt=blog.create_date).last()
+    content['blog'] = blog
     return render(request, 'blog_detail.html', content)
-
-
-def blog_with_type(request, blog_type_pk):
-    content = {}
-    # 获取blog分类的类型
-    blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    content['blogs'] = Blog.objects.filter(blog_type=blog_type)
-    # 通过blog的类型筛选出所有同一个类型的blog
-    content['blogs_count'] = Blog.objects.filter(blog_type=blog_type).count()
-    content['blog_type'] = blog_type
-    content['blog_types'] = BlogType.objects.all()
-    return render(request, 'blog_type.html', content)
-
-
-
